00d7a1a70dcca9744251c03d9939207f147c4ccc
Elyssiah <elyssiah27@gmail.com>
2025-06-04 00:40:45 +0930

    Updates

--- assets/index-CBmAJ5sc.js
+++ assets/index-CBmAJ5sc.js
@@ -62,6 +62,7 @@ tags: ["photography", "analoguephotography", "developingphotos", "darkroom", "fi
 Once the photo paper has been exposed; it needs to go through various chemicals to fully develop into the final picture. Here is the step-by-step process.
 
 ## Tools & Materials :
+
 - Photo to be developed
 - 4 Trays
 - 4 Tongs
@@ -91,8 +92,8 @@ Once the time has passed, use the tongs by this tray to pick up the photo and pl
 title: 'How to Set Up Darkroom Photo Machine (with Height, Exposure and Filters)'
 date: '2025-03-11'
 excerpt: "A darkroom photo enlarger is a machine that allows you to develop photos on light..."
-thumbnail: "/images/blog1.jpg"
-thumbnailPosition: "top"
+thumbnail: "/Lyssis-Designs/Images/Photography/EnlargerHeadOff.jpg"
+thumbnailPosition: "right"
 category: "Photography"
 subcategory: "Analogue Photography Techniques"
 tags: ["photography", "analoguephotography", "photoenlarger", "darkroom"]
@@ -101,6 +102,12 @@ tags: ["photography", "analoguephotography", "photoenlarger", "darkroom"]
 A darkroom photo enlarger is a machine that allows you to develop photos on light sensitive paper from negatives. In this post I will walk you through setting up and using a darkroom photo enlarger.
 
 ## Tools & Materials :
+
+<div class="clearfix">
+<figure class="flex-right" style="width: 15rem; margin-right: 1rem;">
+    <img src="/Lyssis-Designs/Images/Photography/PartsOfDarkroomExposureMachine.jpg">
+    <figcaption>Components of a darkroom photo machine.</figcaption>
+</figure>
 
 - Darkroom photo enlarger
 - Film carrier
@@ -111,21 +118,65 @@ A darkroom photo enlarger is a machine that allows you to develop photos on ligh
 - Safelight
 - Notebook (for recording settings)
 - Negatives
+</div>
 
 ## Process :
 In this tutorial, I will use the proper terminology for the darkroom photo enlarger. If you are unsure as to what I am referring to, please refer to the diagram. Throughout the setup, ensure that all light-sensitive paper is not in any light until taking the actual photo. At TAFE SA, there is a separate draw by each machine that will not let any light through. It is also advised that any electronic devices stay out of the room. Ensure the room is fully light proof with block-out doors, curtains, materials, etc.
 
-Turn on the overhead safelights (red lights), as these need to be on to see, and will not expose the light-sensitive (unless left for many, many hours). Test the exposure timer at 3 seconds (this time can vary as needed). Count to ensure the timer is at three seconds, as this overtime can become inaccurate. For the timer that we had available, there was a knob and 3 buttons. The knob is used to adjust the amount of time that the light on the photo enlarger is on. The three buttons each have a coloured dot. From left to right: red, white and green. Pressing the green button turns on the machine light for the duration of the timer. Pressing the green and white light together turns on the machine light indefinitely. Pressing the red button turns off the light and resets the timer.
+<div class="clearfix">
+<figure class="flex-left" style="width: 10rem;">
+    <img src="/Lyssis-Designs/Images/Photography/Safelights.jpg">
+    <figcaption>Main lights off, safelights on.</figcaption>
+</figure>
+
+### Safelights and Timers :
+Turn on the overhead safelights (red lights), as these need to be on to see, and will not expose the light-sensitive (unless left for many, many hours). Test the exposure timer at 3 seconds (this time can vary as needed). Count to ensure the timer is at three seconds, as this overtime can become inaccurate. For the timer that we had available, there was a knob and 3 buttons. The knob is used to adjust the amount of time that the light on the photo enlarger is on. The three buttons each have a coloured dot. From left to right: red, white and green. Pressing the green button turns on the machine light for the duration of the timer. Pressing the green and white light together turns on the machine light indefinitely. Pressing the red button turns off the light and resets the timer. See the three photos below for examples.
+</div>
+
+<div class="clearfix">
+<div class="timer-gallery">
+  <figure>
+    <img src="/Lyssis-Designs/Images/Photography/TimerOff.jpg" alt="Timer off">
+    <figcaption>Timer in the off position.</figcaption>
+  </figure>
+  <figure>
+    <img src="/Lyssis-Designs/Images/Photography/TimerCountdown.jpg" alt="Timer countdown">
+    <figcaption>Timer counting down.</figcaption>
+  </figure>
+  <figure>
+    <img src="/Lyssis-Designs/Images/Photography/TimerOn.jpg" alt="Timer on">
+    <figcaption>Timer continuously on.</figcaption>
+  </figure>
+</div>
+</div>
+
+<div class="clearfix">
+
+### The Elevation Knob :
+<figure class="flex-right" style="width: 15rem;">
+    <img src="/Lyssis-Designs/Images/Photography/ElevationKnob.jpg">
+    <figcaption>Closeup of the elevation knob.</figcaption>
+</figure>
+
 
 Use the elevation knob to adjust the height of the machine. The higher the enlarger head, the larger the projection, but also the dimmer and less sharp the final image can become. In my experimentations, I always had it at 40cm.
+</div>
 
-<b>Optional: Adding Filters</b>
+### Optional - Adding Filters :
 
-Filters can be used to adjust the sharpness and tone. They typically come in a packet number 00 (low contrast) to 5 (high contrast). These filters slide under the light source and above the film carrier. Ensure the machine light is off before sliding the filter in.
+- Filters can be used to adjust the sharpness and tone. They typically come in a packet number 00 (low contrast) to 5 (high contrast). These filters slide under the light source and above the film carrier. Ensure the machine light is off before sliding the filter in.
 
+### Focusing the Image :
 Turn off the main lights (not the safelights). To focus the image, start by placing the negative onto the film carrier and turning on the machine. Adjust the focus knob the see a clear image projected onto the easel. If you are instead using another technique such as OHP paper or objects, this step is not required.
 
-Set the aperture to most open lens (brightest setting). Now adjust the aperture to the right by 4 clicks (which can be felt when gently adjusting the aperture lens).
+### Setting the Aperture :
+<div class="clearfix">
+<figure class="flex-left" style="width: 10rem;">
+    <img src="/Lyssis-Designs/Images/Photography/EnlargerLens.jpg">
+    <figcaption>The aperture lens on the darkroom photo enlarger.</figcaption>
+</figure>
+Set the aperture lens to fully open (brightest setting). Now adjust the aperture to the right by 4 clicks (which can be felt when gently adjusting the aperture lens).
+</div>
 
 A printable checklist of the above is also available.
 
@@ -745,35 +796,32 @@ Technically I started these earrings with experimentation using the rolling mill
 
 I then when home and sketched some designs and decided I wanted to do a butterfly-like design.
 
-<figure class="float-right" style="width: 20rem; margin-left: 1rem;">
-    <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringsRollingMillPrep.jpg"
-    style="width: 100%;">
-    <figcaption>Textured ribbon glued to aluminium. Ready to go through the rolling mill.</figcaption>
+<div class="clearfix">
+<figure class="flex-right" style="max-width: 20rem; margin-left: 1rem;">
+  <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringsRollingMillPrep.jpg">
+  <figcaption>Textured ribbon glued to aluminium. Ready to go through the rolling mill.</figcaption>
 </figure>
-
 I started by cutting the aluminium sheet into 3cm x 3cm squares. I then glued the ribbon in a way that could created a butterfly once cut out. Once the glue had dried, the aluminium went through the rolling mill. See [Roller Printing](#/blog/Jewellery/Silversmithing-Techniques/Roller-Printing) for how to use a rolling mill.
+</div>
 
-<div class="clear"></div>
-
-<figure class="figure-left-small" style="width: 7rem; margin-left: 1rem;">
-    <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSawed1.jpg"
-    style="width: 100%;">
-    <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSawed2.jpg"
-    style="width: 100%;">
-    <figcaption>Sawing out the butterflies.</figcaption>
+<div class="clearfix">
+<figure class="side-by-side" style="max-width: 300px;">
+  <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSawed1.jpg" alt="Sawed butterfly 1">
+  <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSawed2.jpg" alt="Sawed butterfly 2">
+  <figcaption style="text-align: center;">Sawing out the butterflies.</figcaption>
 </figure>
 
 Next, I removed the leftover ribbon and drew a butterfly onto the aluminium. These designs were cut out using the jewellers saw. See [Sawing Metal for Jewellery](#/blog/Jewellery/Silversmithing-Techniques/Sawing-Metal-for-Jewellery) to learn how to use a jewellers saw.
-<div class="clear"></div>
+</div>
 
-
-<figure class="float-right" style="width: 20rem; margin-right: 1rem;">
-    <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSanded.jpg"
-    style="width: 100%;">
+<div class="clearfix">
+<figure class="flex-right" style="width: 20rem; margin-right: 1rem;">
+    <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSanded.jpg">
     <figcaption>Filing & sanding the butterflies.</figcaption>
 </figure>
 
 The butterflies were not symmetrical, but I was already trying to achieve this organic shape, so I was very happy with it. The edges were tidied with the needle files and then further smoothed with the emery sticks. See [Filing Metal Edges](#/blog/Jewellery/Silversmithing-Techniques/Filing-Metal-Edges) to learn how.
+</div>
 
 The top corner of a wing on each butterfly was marked about 5mm in. I took the butterflies over to the drill press and carefully lowered the drill into the metal. Once the hole was drilled, emery sticks were used to clean up the rough edges.
 
@@ -786,7 +834,7 @@ The final step to create my butterflies was to make the hooks. See how I did thi
   <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringBackFinal.jpg"
        alt="Back view of the butterfly earrings"
        style="width: 100%; max-width: 300px; cursor: zoom-in">
-  <figcaption style="width: 100%; text-align: center;">Front & back views of the butterfly earrings</figcaption>
+  <figcaption style="width: 100%; text-align: center;">Front & back views of the butterfly earrings.</figcaption>
 </figure>
 `,Mw=`---
 title: 'Roller Printing Experimentation'
