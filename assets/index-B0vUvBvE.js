cc2df2dd58e400397144558a1ef9bb10728e6db2
Elyssiah <elyssiah27@gmail.com>
2025-06-05 20:10:43 +0930

    Updates

--- assets/index-B0vUvBvE.js
+++ assets/index-B0vUvBvE.js
@@ -177,9 +177,10 @@ Turn on the overhead safelights (red lights), as these need to be on to see, and
 </div>
 </div>
 
-<div class="clearfix">
+<br>
 
 ### The Elevation Knob :
+<div class="clearfix">
 <figure class="flex-right" style="width: 15rem;">
     <img src="/Lyssis-Designs/Images/Photography/ElevationKnob.jpg">
     <figcaption>Closeup of the elevation knob.</figcaption>
@@ -824,7 +825,8 @@ These earrings started as an experiment with roller printing. I played around wi
   <img src="/Lyssis-Designs/Images/Jewellery/RollerPrintingButterflyPractice.jpg">
   <figcaption>Textured ribbon printed onto aluminium.</figcaption>
 </figure>
-Technically I started these earrings with experimentation using the rolling mill. I had experimented with many different materials and found a texture from a ribbon that I liked. See [Roller Printing Experimentation](#/blog/Jewellery/Completed-Jewellery/Roller-Printing-Experimentation).
+
+Technically I started these earrings with experimentation using the rolling mill. I had experimented with many different materials and found a texture from a ribbon that I liked. See [Roller Printing Experimentation](#/Blog/Jewellery/Completed-Jewellery/Roller-Printing-Experimentation).
 </div>
 
 <div class="clearfix">
@@ -840,6 +842,7 @@ I then when home and sketched some designs and decided I wanted to do a butterfl
   <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringsRollingMillPrep.jpg">
   <figcaption>Textured ribbon glued to aluminium. Ready to go through the rolling mill.</figcaption>
 </figure>
+
 I started by cutting the aluminium sheet into 3cm x 3cm squares. I then glued the ribbon in a way that could created a butterfly once cut out. Once the glue had dried, the aluminium went through the rolling mill. See [Roller Printing](#/blog/Jewellery/Silversmithing-Techniques/Roller-Printing) for how to use a rolling mill.
 </div>
 
@@ -849,6 +852,7 @@ I started by cutting the aluminium sheet into 3cm x 3cm squares. I then glued th
   <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSawed2.jpg" alt="Sawed butterfly 2">
   <figcaption style="text-align: center;">Sawing out the butterflies.</figcaption>
 </figure>
+
 Next, I removed the leftover ribbon and drew a butterfly onto the aluminium. These designs were cut out using the jewellers saw. See [Sawing Metal for Jewellery](#/blog/Jewellery/Silversmithing-Techniques/Sawing-Metal-for-Jewellery) to learn how to use a jewellers saw.
 </div>
 
@@ -857,6 +861,7 @@ Next, I removed the leftover ribbon and drew a butterfly onto the aluminium. The
     <img src="/Lyssis-Designs/Images/Jewellery/ButterflyEarringSanded.jpg">
     <figcaption>Filing & sanding the butterflies.</figcaption>
 </figure>
+
 The butterflies were not symmetrical, but I was already trying to achieve this organic shape, so I was very happy with it. The edges were tidied with the needle files and then further smoothed with the emery sticks. See [Filing Metal Edges](#/blog/Jewellery/Silversmithing-Techniques/Filing-Metal-Edges) to learn how.
 </div>
 
@@ -923,6 +928,7 @@ For this first experimentation, I drew a star design using a pencil and then cut
   <img src="/Lyssis-Designs/Images/Jewellery/RollerPrintingVinyl.jpg">
   <figcaption>Left: Result of vinyl on aluminium. Right: Uncut vinyl.</figcaption>
 </figure>
+
 The vinyl moved in unexpected ways, causing the star design to be further deteriorated. However, it still resembled a star, so I deemed it to be a success, with hypothesis testing required to fully understand the results achieved.
 </div>
 
@@ -933,6 +939,7 @@ The vinyl moved in unexpected ways, causing the star design to be further deteri
     <img src="/Lyssis-Designs/Images/Jewellery/RolllerPrintingLace.jpg">
     <figcaption>Left: Lace used as die. Middle: Result of lace on aluminium. Right: Lace unused as die.</figcaption>
 </figure>
+
 For this experiment, I cut out a piece of lace with fabric scissors. I purposely had the lace bigger than the metal sample, as I wanted to see the design go all the way to the edge. I deem this experiment successful, and the design came out as predicted.
 If I were to further explore this technique, I would change the lace proportion when compared to the metal. I would try having the lace smaller than the metal, lace on both sides of the metal, and multiple layers of the lace positioned in different orientations.
 </div>
@@ -944,6 +951,7 @@ If I were to further explore this technique, I would change the lace proportion 
   <img src="/Lyssis-Designs/Images/Jewellery/RollerPrintingTexturedCard.jpg">
   <figcaption>Left: Textured card unused as die. Middle: Result of textured card on aluminium. Right: Textured card used as die.</figcaption>
 </figure>
+
 For this attempt, I used everyday scissors to cut out part of a design in textured card. I glued this down onto the metal before taking it through the rolling mill. I am happy with the result.
 </div>
 
@@ -954,7 +962,8 @@ For this attempt, I used everyday scissors to cut out part of a design in textur
     <img src="/Lyssis-Designs/Images/Jewellery/RollerPrintingRibbon.jpg">
     <figcaption>Left: Textured ribbon used as die. Middle: Result of textured ribbon on aluminium. Right: Textured ribbon unused as die.</figcaption>
 </figure>
-For this experiment, I glued down 2 pieces of textured ribbon onto the metal. I really liked the results and decided to base my [Butterfly Earrings](#/Blog/Posts/Jewellery/Completed-Jewellery/Butterfly-Earrings) on this. I would have liked to have further explored the layout of the ribbon if time was given, in a similar approach to the lace die above.
+
+For this experiment, I glued down 2 pieces of textured ribbon onto the metal. I really liked the results and decided to base my [Butterfly Earrings](#/blog/Jewellery/Completed-Jewellery/Butterfly-Earrings) on this. I would have liked to have further explored the layout of the ribbon if time was given, in a similar approach to the lace die above.
 </div>
 
 ### Paper Cut into a Design (Bear)
@@ -964,6 +973,7 @@ For this experiment, I glued down 2 pieces of textured ribbon onto the metal. I 
   <img src="/Lyssis-Designs/Images/Jewellery/RolllerPrintingPaperDesign.jpg">
   <figcaption>Left: Bear Paper die after use. Right: Result of paper design on aluminium.</figcaption>
 </figure>
+
 For this design, the bear had already been cut out, and I glued it onto the metal. For a simple design, I was happy to see how much detail came through once it went through the rolling mill.
 </div>
 
@@ -974,6 +984,7 @@ For this design, the bear had already been cut out, and I glued it onto the meta
   <img src="/Lyssis-Designs/Images/Jewellery/RolllerPrintingString2.jpg" alt="Rolller Printing String Back">
   <figcaption style="text-align: center;">Left to Right: String unused as die, printed aluminium, string used as die.</figcaption>
 </figure>
+
 For the final experimentation using the rolling mill, I wanted to test seeing a different design coming through on both sides. I wrapped a piece of string around the metal, to then go through the rolling mill. The results were successful, but it is not my preference in design.
 </div>
 
